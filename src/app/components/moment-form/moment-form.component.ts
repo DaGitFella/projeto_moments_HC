177b3d9bc18a69bb0db038d179b1c321afa6d28d
DaGitFella <davibezerra123457@gmail.com>
2024-08-28 16:31:37 -0300

    form validation with reactive forms

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,14 +1,46 @@
 import {Component, Input} from '@angular/core';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-moment-form',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    NgIf
+  ],
   templateUrl: './moment-form.component.html',
   styleUrl: './moment-form.component.css'
 })
 
 export class MomentFormComponent {
   @Input() btnText!: string;
+
+  momentForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.momentForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+      image: new FormControl('')
+    });
+  }
+
+  get title() {
+    return this.momentForm.get('title')!;
+  }
+
+  get description() {
+    return this.momentForm.get('description')!;
+  }
+
+  submit() {
+    if(this.momentForm.invalid) {
+      return;
+    }
+    console.log('enviou formulário');
+  }
 
 }
